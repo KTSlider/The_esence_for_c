a2babae622dfb7cb6cf4ea52fcd689df476ba83d
KTSlider <114855278+KTSlider@users.noreply.github.com>
2022-10-03 02:57:23 +0000

    Create hello_world.c

---
+++ 0x0/hello_world.c
@@ -0,0 +1,12 @@
+#include <stdio.h>  //包含頭文件
+
+int main(void)  //main函數為程式主入口，程式是從主函數開始執行的
+{
+    printf("HELLO WORLD - 2022\n");   //printf = 庫函數，也就是功能模塊，用於打印信息
+
+    return 0;
+}
+
+/* 步驟 */
+// 編譯 + 鏈接 + 運行
+// 一個程式只能有一個main函數
